0f6cedebb3d6c5135d65fc4916001b49f4392429
Discusser <47938380+Discusser@users.noreply.github.com>
2025-06-25 09:37:47 +0200

    Fix issue with access wideners

--- common/src/main/java/io/github/discusser/tntarrows/TNTArrowProjectile.java
+++ common/src/main/java/io/github/discusser/tntarrows/TNTArrowProjectile.java
@@ -51,7 +51,7 @@ public class TNTArrowProjectile extends AbstractArrow {
         try {
             this.tntBlock = (TntBlock) BuiltInRegistries.BLOCK.getValue(location);
         } catch (ClassCastException e) {
-            TNTArrows.LOGGER.info("Tried creating a TNT arrow that has an invalid TNT block: " + location);
+            TNTArrows.LOGGER.warning("Tried creating a TNT arrow that has an invalid TNT block: " + location);
             this.tntBlock = null;
         }
     }
@@ -85,7 +85,7 @@ public class TNTArrowProjectile extends AbstractArrow {
             }
         } catch (ClassCastException e) {
             if (this.tntBlock != null) {
-                TNTArrows.LOGGER.info("Tried exploding a TNT arrow that has an invalid TNT block: " +
+                TNTArrows.LOGGER.warning("Tried exploding a TNT arrow that has an invalid TNT block: " +
                         this.tntBlock.arch$registryName());
             }
             this.remove(RemovalReason.DISCARDED);
--- neoforge/src/main/java/io/github/discusser/tntarrows/neoforge/TNTArrowsNeoForge.java
+++ neoforge/src/main/java/io/github/discusser/tntarrows/neoforge/TNTArrowsNeoForge.java
@@ -4,14 +4,13 @@ import io.github.discusser.tntarrows.TNTArrows;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(TNTArrows.MOD_ID)
 public final class TNTArrowsNeoForge {
     public TNTArrowsNeoForge(IEventBus modEventBus, ModContainer container) {
-        NeoForge.EVENT_BUS.addListener(TNTArrowsNeoForgeEventHandler::commonSetup);
-        NeoForge.EVENT_BUS.addListener(TNTArrowsNeoForgeEventHandler::buildContents);
-        NeoForge.EVENT_BUS.addListener(TNTArrowsNeoForgeEventHandler::registerItemColors);
+        modEventBus.addListener(TNTArrowsNeoForgeEventHandler::commonSetup);
+        modEventBus.addListener(TNTArrowsNeoForgeEventHandler::buildContents);
+        modEventBus.addListener(TNTArrowsNeoForgeEventHandler::registerItemColors);
 
         // Run our common setup.
         TNTArrows.init();
